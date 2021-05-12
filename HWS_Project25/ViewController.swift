907da0302ccdfa00b28a6a12de37032ff6157e52
nofprog <k.jin3217@gmail.com>
2021-05-12 21:41:53 +0900

    Protocol, send, error

--- HWS_Project25/ViewController.swift
+++ HWS_Project25/ViewController.swift
@@ -10,34 +10,7 @@ import MultipeerConnectivity
 
 class ViewController: UICollectionViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     
-//    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-//        <#code#>
-//    }
-//
-//    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-//        <#code#>
-//    }
-//
-//    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-//        <#code#>
-//    }
-//
-//    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-//        <#code#>
-//    }
-//
-//    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
-//        <#code#>
-//    }
-//
-//    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-//        <#code#>
-//    }
-//
-//    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-//        <#code#>
-//    }
-//
+
     
     var peerID = MCPeerID(displayName: UIDevice.current.name)
     var mcSession: MCSession?
@@ -96,6 +69,26 @@ class ViewController: UICollectionViewController,UINavigationControllerDelegate,
         
         images.insert(image, at: 0)
         collectionView.reloadData()
+        
+        //sessionのnilチェック
+        guard let mcSession = mcSession else { return }
+        //peerがあるか確認
+        if mcSession.connectedPeers.count > 0{
+            //画像をデータ型に変換
+            if let imageData = image.pngData() {
+                //peerを送信
+                do {
+                    try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch {
+                    //エラーの場合はアラートでメッセージを表示
+                    let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    present(ac, animated: true, completion: nil)
+                }
+            }
+            
+        }
+        
     }
     
     func startHosting(action: UIAlertAction){
@@ -110,6 +103,58 @@ class ViewController: UICollectionViewController,UINavigationControllerDelegate,
         mcBrowser.delegate = self
         present(mcBrowser, animated: true, completion: nil)
     }
-
+    
+    
+    //MCSessionDelegate, MCBrowserViewControllerDelegateに必要なプロトコル
+    
+    //sessionに接続、切断すると呼び出される
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Conncted: \(peerID.displayName)")
+        case .connecting:
+            print("Connecting\(peerID.displayName)")
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+            
+        @unknown default:
+            print("Unknown state recieved: \(peerID.displayName)")
+        }
+        
+    }
+    
+    //データを受信すると呼び出される
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        DispatchQueue.main.async {
+            [weak self] in
+            if let image = UIImage(data: data) {
+                self?.images.insert(image, at: 0)
+                self?.collectionView.reloadData()
+            }
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        <#code#>
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
